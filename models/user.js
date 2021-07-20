240d1b34cdcd19c999728debabe742bd3e595b24
Jeral <jeral.cris@gmail.com>
2021-07-20 14:52:43 -0600

    limitamos la informacion del usuario a motrar en la peticion

--- models/user.js
+++ models/user.js
@@ -12,4 +12,14 @@ const UserSchema = Schema({
    active: Boolean,
 });
 
+// Limitamos la información de la petición a mostrar
+UserSchema.methods.toJSON = function () {
+   const { __v, password, _id, ...usuario } = this.toObject();
+
+   //    Remplazamos el nombre de _id por uid
+   usuario.uid = _id;
+
+   return usuario;
+};
+
 module.exports = model('User', UserSchema);
