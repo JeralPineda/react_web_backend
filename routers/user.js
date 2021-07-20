380eaee0dcf88b6ee4f6c3184e9a419e4bc29583
Jeral <jeral.cris@gmail.com>
2021-07-20 14:36:52 -0600

    middleware y express validator para validar los cambos

--- routers/user.js
+++ routers/user.js
@@ -4,6 +4,7 @@
 */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
 
 const { signUp } = require('../controllers/user');
 const { validarCampos } = require('../middleware/validar-campos');
@@ -12,7 +13,14 @@ const router = Router();
 
 router.post(
    '/signup',
-
+   [
+      // middlewares
+      check('name', 'El nombre es obligatorio').not().isEmpty(),
+      check('lastName', 'El nombre es obligatorio').not().isEmpty(),
+      check('email', 'El correo es obligatorio').isEmail(),
+      check('password', 'El password debe contener al menos 6 caracteres').isLength({ min: 6 }),
+      validarCampos,
+   ],
    signUp
 );
 
