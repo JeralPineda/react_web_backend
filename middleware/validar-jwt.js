956dc874469922756a9893a17430c2fb24a48838
Jeral <jeral.cris@gmail.com>
2021-08-14 22:52:06 -0600

    verificando las fechas de creacion y expiracion del token

--- middleware/validar-jwt.js
+++ middleware/validar-jwt.js
@@ -20,10 +20,10 @@ const validarJWT = (req, res = response, next) => {
    }
 
    try {
-      const { uid, name } = jwt.verify(token, process.env.SECRET_JWT_SEED);
+      const { uid, iat, exp } = jwt.verify(token, process.env.SECRET_JWT_SEED);
 
       req.uid = uid;
-      req.name = name;
+      //   req.name = name;
    } catch (error) {
       console.log(error);
       res.status(401).json({
