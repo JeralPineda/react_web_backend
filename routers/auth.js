b65c1ed2abdbc46f6082ca3b559b389daa6b7d97
Jeral <jeral.cris@gmail.com>
2021-08-14 22:23:14 -0600

    agregando express-validator para los campos en la ruta del login

--- routers/auth.js
+++ routers/auth.js
@@ -1,9 +1,20 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 
 const { signIn } = require('../controllers/auth');
+const { validarCampos } = require('../middleware/validar-campos');
 
 const router = Router();
 
-router.post('/signin', signIn);
+router.post(
+   '/signin',
+   [
+      //
+      check('email', 'El correo es obligatorio').isEmail(),
+      check('password', 'El password debe contener al menos 6 caracteres').isLength({ min: 6 }),
+      validarCampos,
+   ],
+   signIn
+);
 
 module.exports = router;
