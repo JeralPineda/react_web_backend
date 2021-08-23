32b33f1f98d143b8c80b54948e64bf891a498359
Jeral <jeral.cris@gmail.com>
2021-08-23 14:25:22 -0600

    cambiando el nombre archivo por avatar

--- helpers/subir-archivo.js
+++ helpers/subir-archivo.js
@@ -3,8 +3,8 @@ const { v4: uuidv4 } = require('uuid');
 
 const subirArchivo = (files, extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'], carpeta = '') => {
    return new Promise((resolve, reject) => {
-      const { archivo } = files;
-      const nombreCortado = archivo.name.split('.');
+      const { avatar } = files;
+      const nombreCortado = avatar.name.split('.');
       const extension = nombreCortado[nombreCortado.length - 1];
 
       // Validar la extension
@@ -15,7 +15,7 @@ const subirArchivo = (files, extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'],
       const nombreTemp = uuidv4() + '.' + extension;
       const uploadPath = path.join(__dirname, '../uploads/', carpeta, nombreTemp);
 
-      archivo.mv(uploadPath, (err) => {
+      avatar.mv(uploadPath, (err) => {
          if (err) {
             reject(err);
          }
--- middleware/validar-archivo.js
+++ middleware/validar-archivo.js
@@ -2,7 +2,7 @@ const { response } = require('express');
 
 const validarArchivoSubir = (req, res = response, next) => {
    // Validando que venga el archivo desde req.files
-   if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+   if (!req.files || Object.keys(req.files).length === 0 || !req.files.avatar) {
       return res.status(400).json({ msg: 'No hay archivos que subir' });
    }
 
