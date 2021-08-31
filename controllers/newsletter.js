731b4fc8e86fe18b4deae0794b12d3e589c2323f
Jeral <jeral.cris@gmail.com>
2021-08-31 15:35:20 -0600

    Creando endpoint para guardar os emails de la newsletter en la BD

--- controllers/newsletter.js
+++ controllers/newsletter.js
@@ -1,18 +1,44 @@
+const { response } = require('express');
 const Newsletter = require('../models/newsletter');
 
-const suscribeEmail = (req, res = response) => {
+const suscribeEmail = async (req, res = response) => {
    const { email } = req.params;
 
+   if (!email) {
+      return res.status(400).json({
+         code: 400,
+         msg: 'El email es obligatorio',
+      });
+   }
+
    try {
-      res.json({
-         email,
-         msg: 'Newsletter',
+      let newsletters = await Newsletter.findOne({ email });
+
+      //   verificar que el usuario existe
+      if (newsletters) {
+         return res.status(400).json({
+            msg: 'Error al registrar en la newsletter',
+         });
+      }
+
+      const data = {
+         email: email.toLowerCase(),
+      };
+
+      //   creamos nueva suscripción
+      newsletters = new Newsletter(data);
+
+      //   grabar en la base de datos
+      await newsletters.save();
+
+      res.status(201).json({
+         msg: 'Email registrado correctamente',
       });
    } catch (error) {
       console.log(error);
 
       res.status(500).json({
-         msg: 'Hable con el administrador',
+         msg: 'El email ya existe',
       });
    }
 };
