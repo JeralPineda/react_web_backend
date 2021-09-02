dde304511870574cb868229aff99fc1fcc9b3f34
Jeral <jeral.cris@gmail.com>
2021-09-02 17:38:13 -0600

    modelo de datos de los cursos

---
+++ models/courses.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+
+const CourseSchema = Schema({
+   idCourse: {
+      type: Number,
+      unique: true,
+   },
+   link: {
+      type: String,
+   },
+   coupon: {
+      type: String,
+   },
+   price: {
+      type: Number,
+   },
+   order: {
+      type: Number,
+   },
+});
+
+// Limitamos la información de la petición a mostrar
+CourseSchema.methods.toJSON = function () {
+   const { __v, _id, ...course } = this.toObject();
+
+   //    Remplazamos el nombre de _id por uid
+   course.uid = _id;
+
+   return course;
+};
+
+module.exports = model('Course', CourseSchema);
