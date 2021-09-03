aa825062627b3248e0f5c26044cd805ccaa5b90f
Jeral <jeral.cris@gmail.com>
2021-09-02 18:10:52 -0600

    limpiando comentarios

--- routers/courses.js
+++ routers/courses.js
@@ -11,9 +11,6 @@ router.post(
    [
       //
       validarJWT,
-      //   check('email', 'El correo es obligatorio').isEmail(),
-      //   check('password', 'El password debe contener al menos 6 caracteres').isLength({ min: 6 }),
-      //   validarCampos,
    ],
    addCourse
 );
