9e41c17446f9bbffb077c66b3863e342ab9cabe0
Jeral <jeral.cris@gmail.com>
2021-09-06 10:14:50 -0600

    finalizando endpint para crear nuevos posts

--- controllers/post.js
+++ controllers/post.js
@@ -1,11 +1,41 @@
 const { request } = require('express');
-// const Post = require('../models/post');
+const Post = require('../models/post');
 
-const addPost = (req = request, res) => {
+const addPost = async (req = request, res) => {
+   const { title, url, description, date } = req.body;
+
+   const data = {
+      title,
+      url,
+      description,
+      date,
+   };
+
    try {
+      const post = new Post(data);
+
+      if (!post) {
+         return res.status(400).json({
+            code: 400,
+            msg: 'No se ha podido crear el post',
+         });
+      }
+
+      const consulta = await Post.findOne({ url });
+
+      if (consulta) {
+         return res.status(400).json({
+            code: 400,
+            msg: `El post con esta url: ${url} ya fue creado`,
+         });
+      }
+
+      //   guardando en la base de datos
+      await post.save();
+
       res.json({
          code: 200,
-         msg: 'add post',
+         msg: 'Post creado correctamente',
       });
    } catch (error) {
       console.log(error);
